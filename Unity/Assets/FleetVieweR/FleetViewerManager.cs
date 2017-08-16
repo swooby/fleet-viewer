a167f300b445ef0eac51227d1f0b0d7c5b05696a
Paul Peavyhouse <pv@swooby.com>
2017-08-15 23:14:26 -0700

    Adding FleetInputManager, etc

---
+++ Unity/Assets/FleetVieweR/FleetViewerManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FleetViewerManager : MonoBehaviour
+{
+    //public GameObject LaunchVrHomeButton;
+    public FleetInputManager FleetInputManager;
+
+    void Start()
+    {
+#if !UNITY_ANDROID || UNITY_EDITOR
+        /*
+        if (LaunchVrHomeButton == null)
+        {
+            return;
+        }
+        LaunchVrHomeButton.SetActive(false);
+        */
+#else
+        GvrDaydreamApi.CreateAsync((success) =>
+        {
+            if (!success)
+            {
+                // Unexpected. See GvrDaydreamApi log messages for details.
+                Debug.LogError("GvrDaydreamApi.CreateAsync() failed");
+              }
+        });
+#endif  // !UNITY_ANDROID || UNITY_EDITOR
+    }
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    void Update()
+    {
+        /*
+        if (LaunchVrHomeButton == null || FleetInputManager == null)
+        {
+            return;
+        }
+        LaunchVrHomeButton.SetActive(FleetInputManager.IsCurrentlyDaydream());
+        */
+    }
+#endif  // UNITY_ANDROID && !UNITY_EDITOR
+
+    public void LaunchVrHome()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        GvrDaydreamApi.LaunchVrHomeAsync((success) =>
+        {
+            if (!success)
+            {
+                // Unexpected. See GvrDaydreamApi log messages for details.
+                Debug.LogError("GvrDaydreamApi.LaunchVrHomeAsync() failed");
+            }
+        });
+#endif  // UNITY_ANDROID && !UNITY_EDITOR
+    }
+}
