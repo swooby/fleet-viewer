a684f8b1c06db3ece42fd99796d88dc5a058d473
Paul Peavyhouse <pv@swooby.com>
2017-08-27 23:36:55 -0700

    Starting to add some pre-defined ship names

---
+++ Unity/Assets/FleetVieweR/StarCitizen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class StarCitizen
+{
+    public const string Nox = "Nox";
+    public const string Dragonfly = "Dragonfly";
+    public const string MPUV_Personnel = "MPUV Personnel";
+    public const string MPUV_Cargo = "MPUV Cargo";
+    public const string Razor = "Razor";
+    public const string Origin85x = "85x";
+    public const string Mustang_Alpha = "Mustang Alpha";
+    public const string Aurora_ES = "Aurora ES";
+    public const string Avenger_Stalker = "Avenger Stalker";
+    public const string Terrapin = "Terrapin";
+    public const string Gladius = "Gladius";
+    public const string Buccaneer = "Buccaneer";
+    public const string Eclipse = "Eclipse";
+    public const string Hull_A = "Hull A";
+    public const string Gladiator = "Gladiator";
+    public const string Hurricane = "Hurricane";
+    public const string F7C_Hornet = "F7C Hornet";
+    public const string Herald = "Herald";
+    public const string Origin300i = "300i";
+    public const string Origin315p = "315p";
+    public const string Origin325a = "325a";
+    public const string Origin350r = "350r";
+    public const string F7C_M_Super_Hornet = "F7C-M Super Hornet";
+    public const string Prospector = "Prospector";
+    public const string Sabre = "Sabre";
+    public const string Defender = "Defender";
+    public const string Cutlass_Black = "Cutlass Black";
+    public const string Freelancer = "Freelancer";
+    public const string Esperia_Prowler = "Esperia Prowler";
+    public const string Vanguard_Harbinger = "Vanguard Harbinger";
+    public const string Redeemer = "Redeemer";
+    public const string Polaris = "Polaris";
+}
