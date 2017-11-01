a138b1b354743ff806ba09f316009d0482bc3e0f
Paul Peavyhouse <pv@swooby.com>
2017-11-01 12:10:23 -0700

    Attempting to handle both GvrControllerInput Click and Touch

--- Unity/Assets/FleetVieweR/InputDeviceGvrController.cs
+++ Unity/Assets/FleetVieweR/InputDeviceGvrController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,6 +6,11 @@ namespace RTEditor
 {
     public class InputDeviceGvrController : InputDeviceAbstract
     {
+        public const int INPUT_CLICK = 0;
+        public const int INPUT_TOUCH = 1;
+
+        private static readonly int[] INPUTS = { INPUT_CLICK, INPUT_TOUCH };
+
         private GvrLaserPointer _laserPointer;
         private Vector3 _laserPointerStartPosition;
         private Quaternion _laserPointerStartRotation;
@@ -50,10 +56,19 @@ namespace RTEditor
 
         public override bool IsPressed(int deviceButtonIndex)
         {
-            // TODO:(pv) Handle both Touch+TouchPos & Click+Move...
             bool isPressed = Input.GetMouseButton(deviceButtonIndex);
             //Debug.LogWarning("isPressed A:" + isPressed);
-            isPressed = GvrControllerInput.ClickButton;
+            switch(deviceButtonIndex)
+            {
+                case INPUT_CLICK:
+                    isPressed = GvrControllerInput.ClickButton;
+                    break;
+                case INPUT_TOUCH:
+                    isPressed = GvrControllerInput.IsTouching;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("deviceButtonIndex", "deviceButtonIndex must be INPUT_CLICK or INPUT_TOUCH");
+            }
             //Debug.LogWarning("isPressed B:" + isPressed);
             return isPressed;
         }
@@ -63,7 +78,17 @@ namespace RTEditor
             // TODO:(pv) Handle both Touch+TouchPos & Click+Move...
             bool wasPressedInCurrentFrame = Input.GetMouseButtonDown(deviceButtonIndex);
             //Debug.LogWarning("wasPressedInCurrentFrame A:" + wasPressedInCurrentFrame);
-            wasPressedInCurrentFrame = GvrControllerInput.ClickButtonDown;
+            switch (deviceButtonIndex)
+            {
+                case INPUT_CLICK:
+                    wasPressedInCurrentFrame = GvrControllerInput.ClickButtonDown;
+                    break;
+                case INPUT_TOUCH:
+                    wasPressedInCurrentFrame = GvrControllerInput.TouchDown;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("deviceButtonIndex", "deviceButtonIndex must be INPUT_CLICK or INPUT_TOUCH");
+            }
             //Debug.LogWarning("wasPressedInCurrentFrame B:" + wasPressedInCurrentFrame);
             return wasPressedInCurrentFrame;
         }
@@ -73,7 +98,17 @@ namespace RTEditor
             // TODO:(pv) Handle both Touch+TouchPos & Click+Move...
             bool wasReleasedInCurrentFrame = Input.GetMouseButtonUp(deviceButtonIndex);
             //Debug.LogWarning("wasReleasedInCurrentFrame A:" + wasReleasedInCurrentFrame);
-            wasReleasedInCurrentFrame = GvrControllerInput.ClickButtonUp;
+            switch (deviceButtonIndex)
+            {
+                case INPUT_CLICK:
+                    wasReleasedInCurrentFrame = GvrControllerInput.ClickButtonUp;
+                    break;
+                case INPUT_TOUCH:
+                    wasReleasedInCurrentFrame = GvrControllerInput.TouchUp;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("deviceButtonIndex", "deviceButtonIndex must be INPUT_CLICK or INPUT_TOUCH");
+            }
             //Debug.LogWarning("wasReleasedInCurrentFrame B:" + wasReleasedInCurrentFrame);
             return wasReleasedInCurrentFrame;
         }
@@ -101,9 +136,9 @@ namespace RTEditor
 
         public override bool GetPickRay(Camera camera, out Ray ray)
         {
-            Vector2 mousePosition = Input.mousePosition;
+            //Vector2 mousePosition = Input.mousePosition;
             //Debug.LogWarning("GetPickRay: mousePosition:" + mousePosition);
-            ray = camera.ScreenPointToRay(mousePosition);
+            //ray = camera.ScreenPointToRay(mousePosition);
             //Debug.LogWarning("GetPickRay: ray A:" + ray);
 
             if (_laserPointer == null)
@@ -177,25 +212,24 @@ namespace RTEditor
             // Store the current laserPointerEndPoint position as the previous position for the next frame
             _previousFramePositions[0] = laserPointerEndPoint;
 
-            // Now we will loop through all supported laserPointerEndPoint modes and update the mouse offset
+            // Now we will loop through all supported laserPointer inputs and update the input offset
             // since each button was pressed.
-            // TODO:(pv) Handle both Touch+TouchPos & Click+Move...
-            //for (int mouseBtnIndex = 0; mouseBtnIndex < 3; ++mouseBtnIndex)
+            foreach (int input in INPUTS)
             {
-                // If the button was pressed or released in the current frame, there is nothing to
+                // If the input was pressed or released in the current frame, there is nothing to
                 // do except to reset the corresponding offset to the zero vector.
-                if (WasPressedInCurrentFrame(0) ||
-                    WasReleasedInCurrentFrame(0))
+                if (WasPressedInCurrentFrame(input) ||
+                    WasReleasedInCurrentFrame(input))
                 {
-                    _deltaSincePressed[0] = Vector2.zero;
-                    //continue;
+                    _deltaSincePressed[input] = Vector2.zero;
+                    continue;
                 }
                 else
                 {
-                    // If the mouse button wasn't pressed or released, we will check to see if it is
+                    // If the input wasn't pressed or released, we will check to see if it is
                     // still pressed (it may have been pressed in a previous frame). If it is, we will
                     // add the delta since the last frame to get the delta since it was pressed.
-                    if (IsPressed(0)) _deltaSincePressed[0] += _deltaSinceLastFrame[0];
+                    if (IsPressed(input)) _deltaSincePressed[input] += _deltaSinceLastFrame[input];
                 }
             }
         }
