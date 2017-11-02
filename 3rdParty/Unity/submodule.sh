4947f0d00696cdc2de8fcb4f0a09c1507db6a01f
Paul Peavyhouse <pv@swooby.com>
2017-11-02 14:55:23 -0700

    Fixing submodule.sh script

--- 3rdParty/Unity/submodule.sh
+++ 3rdParty/Unity/submodule.sh
@@ -1,5 +1,5 @@
 rm ../../Unity/Assets/Runtime\ Transform\ Gizmos
-ln -s Runtime\ Transform\ Gizmos/Asset ../../Unity/Assets/Runtime\ Transform\ Gizmos
+ln -s ../../3rdParty/Unity/Runtime\ Transform\ Gizmos/Asset/ ../../Unity/Assets/Runtime\ Transform\ Gizmos
 
 rm ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
-ln -s Runtime\ Transform\ Gizmos/GoogleVR/ ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
+ln -s ../../3rdParty/Unity/Runtime\ Transform\ Gizmos/GoogleVR ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
