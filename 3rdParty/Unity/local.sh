8b20adb65ae1357350f77f868e424a5d8bbbe62f
Paul Peavyhouse <pv@swooby.com>
2017-11-02 13:36:14 -0700

    Hooking up Runtime Transform Gizmos submodule

---
+++ 3rdParty/Unity/local.sh
@@ -0,0 +1,5 @@
+rm ../../Unity/Assets/Runtime\ Transform\ Gizmos
+ln -s ~/Development/BitBucket/swooby/runtime_transform_gizmos/Asset/ ../../Unity/Assets/Runtime\ Transform\ Gizmos
+
+rm ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
+ln -s ~/Development/BitBucket/swooby/runtime_transform_gizmos/GoogleVR/ ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
---
+++ 3rdParty/Unity/submodule.sh
@@ -0,0 +1,5 @@
+rm ../../Unity/Assets/Runtime\ Transform\ Gizmos
+ln -s Runtime\ Transform\ Gizmos/Asset ../../Unity/Assets/Runtime\ Transform\ Gizmos
+
+rm ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
+ln -s Runtime\ Transform\ Gizmos/GoogleVR/ ../../Unity/Assets/Runtime\ Transform\ Gizmos\ GoogleVR
