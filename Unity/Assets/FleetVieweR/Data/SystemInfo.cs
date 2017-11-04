3505ffd0afc1ff2df2636758389d85fe01bb8f08
Paul Peavyhouse <pv@swooby.com>
2017-11-04 01:19:36 -0700

    Adding SheetID to SystemInfo

--- Unity/Assets/FleetVieweR/Data/SystemInfo.cs
+++ Unity/Assets/FleetVieweR/Data/SystemInfo.cs
@@ -8,14 +8,17 @@ namespace FleetVieweR
     {
         public const string FIELD_NAME = "Name";
         public const string FIELD_CONFIG_PATH = "Config Path";
+        public const string FIELD_SHEET_ID = "Sheet ID";
 
         public string Name { get; private set; }
         public string ConfigPath { get; private set; }
+        public string SheetId { get; private set; }
 
         public SystemInfo(Dictionary<string, string> dictionary)
         {
             Name = dictionary[FIELD_NAME];
             ConfigPath = dictionary[FIELD_CONFIG_PATH];
+            SheetId = dictionary[FIELD_SHEET_ID];
         }
     }
 }
